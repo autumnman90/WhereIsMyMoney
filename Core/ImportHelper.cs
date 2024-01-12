0d2b750cc12dfefcdb54d0cda8035fa239a17709
Matthäus Herbst <matthaeus.sobik@gmx.de>
2024-01-12 16:08:05 +0100

    Added ImportHeper Class

---
+++ Core/ImportHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereIsMyMoney.Core
+{
+    public class ImportHelper
+    {
+        private static readonly ImportHelper instance = new ImportHelper();
+        public static ImportHelper Instance {  get { return instance; } }
+
+        private int[] headerPositions;
+
+        public ImportHelper()
+        {
+            
+        }
+    }
+}
